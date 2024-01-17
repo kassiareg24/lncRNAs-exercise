cf6446c0e697e49e69fa858d8b7f90e7920bc1a3
Ian <icoccimi@gmail.com>
2024-01-16 22:31:45 -0800

    Updated some images and README, as well as the main script to include a volcano plot analysis.

--- README.md
+++ README.md
@@ -1,11 +1,45 @@
-# lncRNA Analysis in Exercise
 
-This repository serves to analyze the changes in long non-coding RNAs (lncRNAs) after an acute bout of aerobic or resistance exercise. 
+<!-- README.md is generated from README.Rmd. Please edit that file -->
 
-Exercise induces a variety of macroscopic physiological changes, and these are often a result of changes in cellular gene expression. Though there has been a great deal of research given to changes in protein-coding genes, the non-coding regions of DNA can play a substantial role in regulating gene expression.
+# Analysis of lncRNAs after acute exercise
 
-This project is a collaboration between the Muscle Physiology Lab at Queen's and the Clarke Laboratory for Quantitative Exercise Biology.
+## Background
 
-# Usage
-- Install the required R packages
-- Run the script 'main.R' to download the GEO dataset, perform DESeq2 analysis, and generate the manuscript figures.
+This repository serves to analyze the changes in long non-coding RNAs
+(lncRNAs) after an acute bout of aerobic or resistance exercise.
+
+Exercise induces a multitude of physiological changes, and many of these
+are downstream of changes in cellular gene expression. Though there has
+been a great deal of study into protein-coding genes, there has been
+little research into the response of non-coding RNAs in response to
+exercise. Here, we seek to address this gap. We examine this problem
+along two different dimensions - exercise modality and exercise time,
+starting with aerobic exercise.
+
+<figure>
+<img src="images/lncRNA_aerobic_volcano.png"
+alt="lncRNAs after Aerobic Exercise" />
+<figcaption aria-hidden="true">lncRNAs after Aerobic
+Exercise</figcaption>
+</figure>
+
+Then, we consider resistance exercise for both short- and long-term gene
+expression.
+
+<figure>
+<img src="images/lncRNA_resistance_volcano.png"
+alt="lncRNAs after Resistance Exercise" />
+<figcaption aria-hidden="true">lncRNAs after Resistance
+Exercise</figcaption>
+</figure>
+
+This project is a collaboration between the [Muscle Physiology
+Lab](https://skhs.queensu.ca/qmpl/) at Queen’s University and the
+[Clarke Laboratory for Quantitative Exercise
+Biology](https://www.sfu.ca/clarkelab-bpk.html) at Simon Fraser
+University.
+
+## Usage
+
+To use this package, simply run the `main.R` script in R version 4.3.0
+or higher.
--- main.R
+++ main.R
@@ -7,7 +7,10 @@ library(ggplot2)
 library(biomaRt)
 library(pheatmap)
 library(RColorBrewer)
+library(ggrepel)
+library(patchwork)
 
+write_results = FALSE
 pdata_dson <- getGEO("GSE107934")[[1]]
 citation("GEOquery")
 
@@ -54,8 +57,8 @@ dds_dson <- DESeqDataSetFromMatrix(countData = cts_dson,
                        design = ~treatment)
 
 # Filtering all genes that have less than 5 reads across samples
-keep <- rowSums(counts(dds_dson)) > 1
-# keep <- rowSums(counts(dds_dson) >= 5)
+# keep <- rowSums(counts(dds_dson)) > 1
+keep <- rowSums(counts(dds_dson)>=5) >= 5
 dds <-dds_dson[keep,]
 
 dds. <- DESeq(dds)
@@ -89,11 +92,15 @@ generate_MA_plots <- function(res, xlim=c(1,1e5), ylim=c(-11,11)) {
     mtext(res$exp_name, side = 3, line = -1, outer = TRUE, cex=1,font=2)
 }
 
+# You'll need to have the 'apeglm' package for this to work properly.
 res_ae_1 <- generate_deseq_results(dds., name="treatment_AE_1_vs_Baseline", coef=2)
 res_ae_4 <- generate_deseq_results(dds., name="treatment_AE_4_vs_Baseline", coef=3)
 res_re_1 <- generate_deseq_results(dds., name="treatment_RE_1_vs_Baseline", coef=4)
 res_re_4 <- generate_deseq_results(dds., name="treatment_RE_4_vs_Baseline", coef=5)
 summary(res_ae_1$apeglm_results, alpha =0.05)
+summary(res_ae_4$apeglm_results, alpha =0.05)
+summary(res_re_1$apeglm_results, alpha =0.05)
+summary(res_re_4$apeglm_results, alpha =0.05)
 
 # AE 1 hour
 generate_MA_plots(res=res_ae_1)
@@ -129,56 +136,174 @@ my_annotations_dickinson = function(x3) {
 
 # Note, if this fails, use the following version of dbplyr:
 # devtools::install_version("dbplyr", version = "2.3.4")
-my_ae.1hr_df <- DE_results(res_ae_1$apeglm_results)
-my_ae.1hr_df_anno <- my_annotations_dickinson(my_ae.1hr_df)
-final.ae_1hr <- left_join(my_ae.1hr_df, my_ae.1hr_df_anno,
-                          by=c("ensgene"="ensembl_gene_id"))
+ae.1hr_df <- DE_results(res_ae_1$apeglm_results)
+ae.1hr_df_anno <- my_annotations_dickinson(ae.1hr_df)
+final.ae_1hr <- left_join(ae.1hr_df, ae.1hr_df_anno,
+                          by=c("ensgene"="ensembl_gene_id")) %>%
+    dplyr::select(ensgene, external_gene_name, log2FoldChange, lfcSE, pvalue, padj, gene_biotype)
 
-ae_1.4_my.r_df <- DE_results(res_ae_4$apeglm_results)
-ae_1.4_my.r_df_anno <- my_annotations_dickinson(ae_1.4_my.r_df)
-final.ae_4hr <- left_join(ae_1.4_my.r_df, ae_1.4_my.r_df_anno,
-                          by=c("ensgene"="ensembl_gene_id"))
+ae.4hr_df <- DE_results(res_ae_4$apeglm_results)
+ae.4hr_df_anno <- my_annotations_dickinson(ae.4hr_df)
+final.ae_4hr <- left_join(ae.4hr_df, ae.4hr_df_anno,
+                          by=c("ensgene"="ensembl_gene_id")) %>%
+    dplyr::select(ensgene, external_gene_name, log2FoldChange, lfcSE, pvalue, padj, gene_biotype)
 
-re_b.1.r_df <- DE_results(res_re_1$apeglm_results)
-re_b.1.r_df_anno <- my_annotations_dickinson(re_b.1.r_df)
-final.re_1hr <- left_join(re_b.1.r_df, re_b.1.r_df_anno,
-                          by=c("ensgene"="ensembl_gene_id"))
+re.1hr_df <- DE_results(res_re_1$apeglm_results)
+re.1hr_df_anno <- my_annotations_dickinson(re.1hr_df)
+final.re_1hr <- left_join(re.1hr_df, re.1hr_df_anno,
+                          by=c("ensgene"="ensembl_gene_id")) %>%
+    dplyr::select(ensgene, external_gene_name, log2FoldChange, lfcSE, pvalue, padj, gene_biotype)
 
-re_1.4.r_df <- DE_results(res_re_4$apeglm_results)
-re_1.4.r_df_anno <- my_annotations_dickinson(re_1.4.r_df)
-final.re_4hr <- left_join(re_1.4.r_df, re_1.4.r_df_anno,
-                          by=c("ensgene"="ensembl_gene_id"))
+re.4hr_df <- DE_results(res_re_4$apeglm_results)
+re.4hr_df_anno <- my_annotations_dickinson(re.4hr_df)
+final.re_4hr <- left_join(re.4hr_df, re.4hr_df_anno,
+                          by=c("ensgene"="ensembl_gene_id")) %>%
+    dplyr::select(ensgene, external_gene_name, log2FoldChange, lfcSE, pvalue, padj, gene_biotype)
 
 print_gene_results <- function(){
     cat("---1HR Aerobic Exercise Top DE genes---\n")
-    final.ae_1hr %>% dplyr::select(ensgene, external_gene_name, log2FoldChange, lfcSE, pvalue, padj, gene_biotype) %>% head %>% print
+    final.ae_1hr %>% head %>% print
     cat("\n---4HR Aerobic Exercise Top DE genes---\n")
-    final.ae_4hr %>% dplyr::select(ensgene, external_gene_name, log2FoldChange, lfcSE, pvalue, padj, gene_biotype) %>% head %>% print
+    final.ae_4hr %>% head %>% print
     cat("\n---1HR Resistance Exercise Top DE genes---\n")
-    final.re_1hr %>% dplyr::select(ensgene, external_gene_name, log2FoldChange, lfcSE, pvalue, padj, gene_biotype) %>% head %>% print
+    final.re_1hr %>% head %>% print
     cat("\n---4HR Resistance Exercise Top DE genes---\n")
-    final.re_4hr %>% dplyr::select(ensgene, external_gene_name, log2FoldChange, lfcSE, pvalue, padj, gene_biotype) %>% head %>% print
+    final.re_4hr %>% head %>% print
 }
 print_gene_results() # from the supplementary
 
 print_lncRNA_tables <- function() {
     cat("---1HR Aerobic Exercise Top DE LncRNAs---\n")
     final.ae_1hr %>% filter(gene_biotype == "lincRNA" | gene_biotype == "antisense" | gene_biotype == "sense_intronic") %>%
-        dplyr::select(ensgene, external_gene_name, log2FoldChange, lfcSE, pvalue, padj, gene_biotype) %>% print
+        print
     cat("\n---4HR Aerobic Exercise Top DE LncRNAs---\n")
     final.ae_4hr %>% filter(gene_biotype == "lincRNA" | gene_biotype == "antisense" | gene_biotype == "sense_intronic") %>%
-        dplyr::select(ensgene, external_gene_name, log2FoldChange, lfcSE, pvalue, padj, gene_biotype) %>% print
+        print
     cat("\n---1HR Resistance Exercise Top DE LncRNAs---\n")
     final.re_1hr %>% filter(gene_biotype == "lincRNA" | gene_biotype == "antisense" | gene_biotype == "sense_intronic") %>%
-        dplyr::select(ensgene, external_gene_name, log2FoldChange, lfcSE, pvalue, padj, gene_biotype) %>% print
+        print
     cat("\n---4HR Resistance Exercise Top DE LncRNAs---\n")
     final.re_4hr %>% filter(gene_biotype == "lincRNA" | gene_biotype == "antisense" ) %>%
-        dplyr::select(ensgene, external_gene_name, log2FoldChange, lfcSE, pvalue, padj, gene_biotype) %>% print
+        print
 }
 print_lncRNA_tables() # from the main-text
 
-final.ae_1hr %>% dplyr::select(ensgene, external_gene_name, log2FoldChange, lfcSE, pvalue, padj, gene_biotype) %>% head
-final.ae_4hr %>% dplyr::select(ensgene, external_gene_name, log2FoldChange, lfcSE, pvalue, padj, gene_biotype) %>% head
-final.re_1hr %>% dplyr::select(ensgene, external_gene_name, log2FoldChange, lfcSE, pvalue, padj, gene_biotype) %>% head
-final.re_4hr %>% dplyr::select(ensgene, external_gene_name, log2FoldChange, lfcSE, pvalue, padj, gene_biotype) %>% head
+final.ae_1hr <- final.ae_1hr %>% dplyr::select(ensgene, external_gene_name, log2FoldChange, lfcSE, pvalue, padj, gene_biotype)
+final.ae_4hr <- final.ae_4hr %>% dplyr::select(ensgene, external_gene_name, log2FoldChange, lfcSE, pvalue, padj, gene_biotype)
+final.re_1hr <- final.re_1hr %>% dplyr::select(ensgene, external_gene_name, log2FoldChange, lfcSE, pvalue, padj, gene_biotype)
+final.re_4hr <- final.re_4hr %>% dplyr::select(ensgene, external_gene_name, log2FoldChange, lfcSE, pvalue, padj, gene_biotype)
 
+if (!file.exists("proc")) dir.create("proc")
+if (write_results == TRUE) {
+    write_csv(final.ae_1hr, file = "proc/AE_baseline.vs.1.csv", append = FALSE)
+    write_csv(final.ae_4hr, file = "proc/AE_baseline.vs.4.csv", append = FALSE)
+    write_csv(final.re_1hr, file = "proc/RE_baseline.vs.1.csv", append = FALSE)
+    write_csv(final.re_4hr, file = "proc/RE_baseline.vs.4.csv", append = FALSE)
+}
+
+## Extra plots
+# Volcano plot
+
+df_ae_1 <- as.data.frame(res_ae_1$normal_results) %>%
+    dplyr::mutate(threshold = padj<0.05 & abs(log2FoldChange) >= 0.58) %>%
+    subset(!is.na(baseMean)) %>%
+    rownames_to_column(var = "ensgene")
+
+df_ae_4 <- as.data.frame(res_ae_4$normal_results) %>%
+    dplyr::mutate(threshold = padj<0.05 & abs(log2FoldChange) >= 0.58) %>%
+    subset(!is.na(baseMean)) %>%
+    rownames_to_column(var = "ensgene")
+
+df_re_1 <- as.data.frame(res_re_1$normal_results) %>%
+    dplyr::mutate(threshold = padj<0.05 & abs(log2FoldChange) >= 0.58) %>%
+    subset(!is.na(baseMean)) %>%
+    rownames_to_column(var = "ensgene")
+
+df_re_4 <- as.data.frame(res_re_4$normal_results) %>%
+    dplyr::mutate(threshold = padj<0.05 & abs(log2FoldChange) >= 0.58) %>%
+    subset(!is.na(baseMean)) %>%
+    rownames_to_column(var = "ensgene")
+
+anno_DF<- function (df) {
+    anno <- my_annotations_dickinson(tdf)
+    anno_gene <- dplyr::distinct(anno)
+    df$anno <- anno[match(df$ensgene, anno_gene$ensembl_gene_id),]$external_gene_name
+    df$biotype <- anno[match(df$ensgene, anno_gene$ensembl_gene_id),]$gene_biotype
+    df$annoplot <- ifelse(df$threshold == TRUE & (df$biotype == "lincRNA" | df$biotype == "antisense" | df$biotype == "sense_intronic"),
+                          df$anno,
+                           "")
+    df
+}
+
+df_ae_1 <- anno_DF(df_ae_1)
+df_ae_4 <- anno_DF(df_ae_4)
+df_re_1 <- anno_DF(df_re_1)
+df_re_4 <- anno_DF(df_re_4)
+
+
+#anno[tdf$ensgene %in% anno_gene$ensembl_gene_id,]$external_gene_name
+# annoplot <- ifelse(tdf$threshold == FALSE, "", tdf$anno)
+
+p1 <- ggplot(df_ae_1,aes(x = log2FoldChange, y = -log10(padj))) +
+    geom_point(aes(color=threshold)) +
+    geom_text_repel(aes(label = annoplot), max.overlaps = 100,
+                    point.size=NA,
+                    segment.colour = NA) +
+    ggtitle("1hr") +
+    xlim(c(-10, 10)) +
+    xlab("log2 fold change") +
+    ylab("-log10 adjusted p-value") +
+    theme(legend.position = "none",
+          plot.title = element_text(size = rel(1.5), hjust = 0.5),
+          axis.title = element_text(size = rel(1.25)))
+
+p2 <- ggplot(df_ae_4,aes(x = log2FoldChange, y = -log10(padj))) +
+    geom_point(aes(color=threshold)) +
+    geom_text_repel(aes(label = annoplot), max.overlaps = 100,
+                    point.size=NA,
+                    segment.colour = NA) +
+    ggtitle("4hr") +
+    xlim(c(-10, 10)) +
+    xlab("log2 fold change") +
+    ylab("-log10 adjusted p-value") +
+    theme(legend.position = "none",
+          plot.title = element_text(size = rel(1.5), hjust = 0.5),
+          axis.title = element_text(size = rel(1.25)))
+
+(p1 | p2) + plot_annotation(
+    title = 'Long Non-Coding RNAs after Aerobic Exercise',
+)
+
+p3 <- ggplot(df_re_1,aes(x = log2FoldChange, y = -log10(padj))) +
+    geom_point(aes(color=threshold)) +
+    geom_text_repel(aes(label = annoplot), max.overlaps = 100,
+                    point.size=NA,
+                    segment.colour = NA) +
+    ggtitle("1hr") +
+    xlim(c(-10, 10)) +
+    xlab("log2 fold change") +
+    ylab("-log10 adjusted p-value") +
+    theme(legend.position = "none",
+          plot.title = element_text(size = rel(1.5), hjust = 0.5),
+          axis.title = element_text(size = rel(1.25)))
+
+p4 <- ggplot(df_re_4,aes(x = log2FoldChange, y = -log10(padj))) +
+    geom_point(aes(color=threshold)) +
+    geom_text_repel(aes(label = annoplot), max.overlaps = 100,
+                    point.size=NA,
+                    segment.colour = NA) +
+    ggtitle("4hr") +
+    xlim(c(-10, 10)) +
+    xlab("log2 fold change") +
+    ylab("-log10 adjusted p-value") +
+    theme(legend.position = "none",
+          plot.title = element_text(size = rel(1.5), hjust = 0.5),
+          axis.title = element_text(size = rel(1.25)))
+
+(p3 | p4) + plot_annotation(
+    title = 'Long Non-Coding RNAs after Resistance Exercise',
+)
+
+if (!file.exists("images")) dir.create(images)
+
+# Heatmap
